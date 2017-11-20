7b4a95b1877a9760c91f916872b513f14056adc9
LB <mail@lb.ee>
2017-11-20 21:04:26 +0800

    add run.py file

---
+++ run.py
@@ -0,0 +1,29 @@
+import keys
+
+from tropofy import main as tropofy_main, serve_app_cascade
+
+apps_config = {
+    'tropofy': {
+        'api_url': 'https://api.tropofy.com',
+        'auth_url': 'https://auth.tropofy.com',
+    },
+    'database': {
+        'url': 'sqlite:///database.db',
+    },
+    'apps': [
+        {
+            'module': 'main',
+            'classname': 'Application',
+            'config': {
+                'key.public': keys.public,
+                'key.private': keys.private,
+            }
+        }
+    ]
+}
+
+
+tropofy_app = tropofy_main(apps_config)
+
+if __name__ == "__main__":
+    serve_app_cascade(tropofy_app, '0.0.0.0', 8080)

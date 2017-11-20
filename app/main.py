9905dbc7bcdb49942726a9e0d0cabb2cc1cde0c6
LB <mail@lb.ee>
2017-11-20 21:51:12 +0800

    Linting

--- app/main.py
+++ app/main.py
@@ -43,13 +43,19 @@ class KnapsackAllocationWeightPieChart(Chart):
         return {"item": ("string", "Item"), "weight": ("number", "Weight")}
 
     def get_table_data(self, app_session):
-        items_in_knapsack = app_session.data_set.query(KnapsackItem).filter_by(in_knapsack=True).all()
+        items_in_knapsack = app_session.data_set.query(
+            KnapsackItem).filter_by(in_knapsack=True).all()
         ks_weight = sum([item.weight for item in items_in_knapsack])
         ks_max_weight = app_session.data_set.get_param('max_weight')
 
-        items_in_pie = [{"item": item.name, "weight": item.weight} for item in items_in_knapsack]
+        items_in_pie = [
+            {"item": item.name, "weight": item.weight}
+            for item in items_in_knapsack
+        ]
         if ks_weight < ks_max_weight:
-            items_in_pie.append({"item": "Unused", "weight": ks_max_weight - ks_weight})
+            items_in_pie.append(
+                {"item": "Unused", "weight": ks_max_weight - ks_weight}
+            )
         return items_in_pie
 
     def get_column_ordering(self, app_session):
@@ -59,10 +65,17 @@ class KnapsackAllocationWeightPieChart(Chart):
         return "weight"
 
     def get_chart_options(self, app_session):
-        items_in_knapsack = app_session.data_set.query(KnapsackItem).filter_by(in_knapsack=True).all()
+        items_in_knapsack = app_session.data_set.query(
+            KnapsackItem).filter_by(in_knapsack=True).all()
         ks_value = sum(item.value for item in items_in_knapsack)
         ks_weight = sum(item.weight for item in items_in_knapsack)
-        return {'title': 'Knapsack Value: {value}. Weight used: {weight} / {max}'.format(value=ks_value, weight=ks_weight, max=app_session.data_set.get_param('max_weight'))}
+        template = 'Knapsack Value: {value}. Weight used: {weight} / {max}'
+        title = template.format(
+            value=ks_value,
+            weight=ks_weight,
+            max=app_session.data_set.get_param('max_weight')
+        )
+        return {'title': title}
 
 
 class Application(AppWithDataSets):
@@ -74,17 +87,31 @@ class Application(AppWithDataSets):
 
     def get_gui(self):
         step_group1 = StepGroup(name='Enter your Data')
-        step_group1.add_step(Step(name='Knapsack Items', widgets=[SimpleGrid(KnapsackItem)]))
+        step_group1.add_step(
+            Step(name='Knapsack Items', widgets=[SimpleGrid(KnapsackItem)]))
         step_group1.add_step(Step(
             name='Knapsack Weight',
             widgets=[{"widget": ParameterForm(), "cols": 6}],
         ))
 
         step_group2 = StepGroup(name='Solve')
-        step_group2.add_step(Step(name='Solve Knapsack Problem using LocalSolver', widgets=[ExecuteLocalSolver()]))
+        step_group2.add_step(
+            Step(
+                name='Solve Knapsack Problem using LocalSolver',
+                widgets=[ExecuteLocalSolver()]
+            )
+        )
 
         step_group3 = StepGroup(name='View the Solution')
-        step_group3.add_step(Step(name='Knapsack Allocation', widgets=[KnapsackAllocationWeightPieChart(), SimpleGrid(KnapsackItem)]))
+        step_group3.add_step(
+            Step(
+                name='Knapsack Allocation',
+                widgets=[
+                    KnapsackAllocationWeightPieChart(),
+                    SimpleGrid(KnapsackItem)
+                ]
+            )
+        )
 
         return [step_group1, step_group2, step_group3]
 
@@ -95,7 +122,15 @@ class Application(AppWithDataSets):
         }
 
     def get_parameters(self):
-        return [Parameter(name='max_weight', label='Max Knapsack Weight', default=10, allowed_type=int, validator=validate_value_g_zero)]
+        return [
+            Parameter(
+                name='max_weight',
+                label='Max Knapsack Weight',
+                default=10,
+                allowed_type=int,
+                validator=validate_value_g_zero
+            )
+        ]
 
     def get_default_example_data_set_name(self):
         return "Demo French Shopping Knapsack"
@@ -124,42 +159,47 @@ def load_data_set_toy(app_session):
 
 def load_data_set_100(app_session):
     weights = [
-        361, 949, 30, 448, 516, 54, 492, 762, 720, 855, 78, 678, 474, 263, 96, 845,
-        85, 876, 242, 321, 144, 889, 525, 948, 653, 261, 504, 883, 693, 224, 746,
-        246, 38, 388, 603, 863, 126, 654, 939, 636, 961, 376, 556, 731, 199, 692,
-        75, 337, 113, 63, 320, 878, 417, 647, 208, 617, 609, 738, 144, 748, 388,
-        789, 106, 259, 409, 518, 452, 719, 359, 119, 739, 73, 458, 552, 727, 581,
-        298, 663, 136, 221, 268, 774, 118, 670, 906, 153, 790, 244, 902, 242, 224,
-        381, 669, 515, 917, 724, 958, 223, 606, 372
+        361, 949, 30, 448, 516, 54, 492, 762, 720, 855, 78, 678, 474, 263,
+        96, 845, 85, 876, 242, 321, 144, 889, 525, 948, 653, 261, 504, 883,
+        693, 224, 746, 246, 38, 388, 603, 863, 126, 654, 939, 636, 961, 376,
+        556, 731, 199, 692, 75, 337, 113, 63, 320, 878, 417, 647, 208, 617,
+        609, 738, 144, 748, 388, 789, 106, 259, 409, 518, 452, 719, 359,
+        119, 739, 73, 458, 552, 727, 581, 298, 663, 136, 221, 268, 774, 118,
+        670, 906, 153, 790, 244, 902, 242, 224, 381, 669, 515, 917, 724, 958,
+        223, 606, 372
     ]
     values = [
-        537, 566, 320, 654, 668, 68, 332, 298, 689, 964, 731, 726, 299, 22, 798,
-        558, 287, 642, 291, 152, 272, 535, 731, 866, 987, 5, 985, 763, 793, 451,
-        429, 754, 50, 246, 142, 711, 326, 863, 295, 860, 518, 912, 672, 224, 578,
-        779, 917, 872, 836, 542, 987, 594, 914, 358, 359, 479, 617, 959, 441, 76,
-        156, 646, 509, 30, 716, 276, 634, 791, 442, 7, 99, 233, 79, 942, 68, 30,
-        540, 792, 703, 82, 478, 482, 979, 902, 785, 564, 590, 713, 861, 374, 873,
-        316, 14, 692, 954, 603, 381, 835, 735, 779
+        537, 566, 320, 654, 668, 68, 332, 298, 689, 964, 731, 726, 299, 22,
+        798, 558, 287, 642, 291, 152, 272, 535, 731, 866, 987, 5, 985, 763,
+        793, 451, 429, 754, 50, 246, 142, 711, 326, 863, 295, 860, 518, 912,
+        672, 224, 578, 779, 917, 872, 836, 542, 987, 594, 914, 358, 359, 479,
+        617, 959, 441, 76, 156, 646, 509, 30, 716, 276, 634, 791, 442, 7, 99,
+        233, 79, 942, 68, 30, 540, 792, 703, 82, 478, 482, 979, 902, 785, 564,
+        590, 713, 861, 374, 873, 316, 14, 692, 954, 603, 381, 835, 735, 779
     ]
 
     items = []
     for i in range(len(weights)):
-        items.append(KnapsackItem(name="Item_%s" % i, weight=weights[i], value=values[i]))
+        items.append(
+            KnapsackItem(
+                name="Item_%s" % i, weight=weights[i], value=values[i]
+            )
+        )
 
     app_session.data_set.add_all(items)
     app_session.data_set.set_param('max_weight', 24379, app_session.app)
 
 
 def call_local_solver(app_session):
-    invoke_localsolver_using_lsp_file(app_session, write_localsolver_input_file(app_session))
+    invoke_localsolver_using_lsp_file(
+        app_session, write_localsolver_input_file(app_session))
 
 
 def write_localsolver_input_file(app_session):
-    input_file_name = 'input.in'
+    file_name = 'input.in'
 
-    input_file_path = app_session.get_file_path_in_local_data_set_dir(input_file_name)
-    f = open(input_file_path, 'w')
-    print('write_localsolver_input_file', input_file_path, f)
+    file_path = app_session.get_file_path_in_local_data_set_dir(file_name)
+    f = open(file_path, 'w')
 
     items = KnapsackItem.get_ordered_list_of_all_items(app_session.data_set)
     weights = [str(item.weight) for item in items]
@@ -172,7 +212,7 @@ def write_localsolver_input_file(app_session):
     f.write('%s' % app_session.data_set.get_param('max_weight'))
 
     f.close()
-    return input_file_name
+    return file_name
 
 
 def invoke_localsolver_using_lsp_file(app_session, input_file_name):
@@ -180,30 +220,36 @@ def invoke_localsolver_using_lsp_file(app_session, input_file_name):
         item.in_knapsack = False  # Reset solution
 
     lsp_file_path = pkg_resources.resource_filename('app', 'solver.lsp')
-    print('invoking local solver', lsp_file_path)
-    solution_file_name = 'output.txt'
-    solution_file_path = app_session.get_file_path_in_local_data_set_dir(solution_file_name)
-    open(solution_file_path, 'w').close()  # clear the solution file if it exists
-    print('command', ["localsolver", lsp_file_path, "inFileName=%s" % input_file_name, "solFileName=%s" % solution_file_name, "lsTimeLimit=2"])
+    file_name = 'output.txt'
+    file_path = app_session.get_file_path_in_local_data_set_dir(file_name)
+    open(file_path, 'w').close()  # clear the solution file if it exists
     p = subprocess.Popen(
-        ["localsolver", lsp_file_path, "inFileName=%s" % input_file_name, "solFileName=%s" % solution_file_name, "lsTimeLimit=2"],
+        [
+            "localsolver",
+            lsp_file_path,
+            "inFileName=%s" % input_file_name,
+            "solFileName=%s" % file_name,
+            "lsTimeLimit=2"
+        ],
         cwd=app_session.local_data_set_dir,
         stdout=subprocess.PIPE
     )
-    print('subprocess', p, input_file_name, output_filename)
     out, _ = p.communicate()
-    with open(solution_file_path) as f:
+    with open(file_path) as f:
         content = f.readlines()
         if content:
             app_session.task_manager.send_progress_message(out)
-            items = KnapsackItem.get_ordered_list_of_all_items(app_session.data_set)
+            items = KnapsackItem.get_ordered_list_of_all_items(
+                app_session.data_set)
             item_indexes_in_knapsack = [int(i) for i in content]
             for i, item in enumerate(items):
                 item.in_knapsack = i in item_indexes_in_knapsack
         else:
             app_session.task_manager.send_progress_message(
-                '''The data you have entered exceeds the limits of the trial version of LocalSolver used to run this app.
-                LocalSolver's Trial Version does not allow more than 1000 expressions and 100 decisions.'''
+                '''The data you have entered exceeds the limits of the trial
+                version of LocalSolver used to run this app. LocalSolver's
+                Trial Version does not allow more than 1000 expressions
+                and 100 decisions.'''
             )
 
 

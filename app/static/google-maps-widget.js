48d69d505215350edcce7c73980e298d43366ecb
LB <mail@lb.ee>
2017-11-24 12:31:38 +0800

    add placeholder files for google maps widget

---
+++ app/static/google-maps-widget.js
@@ -0,0 +1,16 @@
+define(['knockout'], function(ko){
+  return function mapsViewModel(params){
+    var self = this;
+      /*
+      params = {
+          spec: data included in the widget's serialise method,
+          step: {
+              parent: see section on scaffold,
+              widgets,
+              stepGroup,
+          }
+      }
+      */
+    return self;
+  }
+});
